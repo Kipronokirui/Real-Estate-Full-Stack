c3e7bd0dfd94f584417222b41b700fa83be1e4eb
Kipronokirui <victorkipronokirui2@gmail.com>
2023-06-23 01:30:25 +0300

    frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
 import React from 'react'
-import './App.css';
-import { Layout } from 'antd';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import AppFooter from './components/common/Footer'
-import AppHeader from './components/common/AppHeader'
-import HomePage from './pages/HomePage';
-import PropertyListPage from './pages/PropertyListPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import NotFound from "./components/NotFound";
+import PropertiesPage from "./pages/PropertiesPage";
 
-const {Content, Header, Footer} = Layout
+
 function App() {
   return (
-    <Router>
-      <Layout className='app-layout'>
-        <Header>
-          <AppHeader />
-        </Header>
-        <Content>
+    <>
+			<Router>
+        <Header />
+        <main className="py-3">
           <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route exact path="/properties" element={<PropertyListPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route
+							path="/properties"
+							element={<PropertiesPage />}
+            />
+            <Route path="*" element={<NotFound />} />
           </Routes>
-        </Content>
-        <Footer>
-          <AppFooter />
-        </Footer>
-      </Layout>
-    </Router>
+          <ToastContainer theme="dark" />
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
